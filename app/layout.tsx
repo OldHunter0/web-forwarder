2fcc6bcb6af54589300785fb9699b9b4a9083d99
fuyuan <sx0201asd@gmail.com>
2025-01-26 19:42:17 +0800

    fix

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: '重定向页面',
+  description: '重定向到博客页面',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="zh">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+} 
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      <h1>重定向中...</h1>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">redirect to 老猎人‘s blog...</h1>
+    </main>
   )
 } 
